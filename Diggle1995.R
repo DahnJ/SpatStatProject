3b076df393db9809a52ccd2504fa1287e6b1f9fa
DahnJ <dahnjahn@gmail.com>
2019-01-14 02:12:35 +0100

    Pre-final version

--- Diggle1995.R
+++ Diggle1995.R
@@ -1,50 +1,12 @@
 ### Analysis of Diggle's 1995 paper ###
 
-## 1: Intro
-# Spatial clustering: nonuniform geopgraphical distribution
-# Temporal clustering: more illness in winter
-# But unless there's a contagious element or spatio-temporally localised increases, these two operate separately
-
-# Comparison to Knox - counting pairs that are within [0,s]x[0,t] of each other
-# Similar results, except here it's a function of (s,t)
-
-## 2: Spatio-temporal processes
-# K(s,t) = 2*pi*s^2*t for homoPoisson
-# If space-time independence, then K(s,t)=K(s)K(t)
-# SO-intensity function l_2(s,t) - density at distance (s,t)
-# K(s,t) - cummulative number till distance (s,t)
-
-## 3: Estimation of second-order properties
-# Why do the estimates look this way? Why is there {n(n-1)}^-1 just once?
-# Edge-effects less important in temporal scale - why?
-# That's why they define such a crude correction
-# w,v=1 => ~Knox
-
-## 4: Diagnostic for space-time clustering
-# D = K - K1*K2
-# D0 = D/(K1*K2) = K/(K1*K2) - 1
-# Plot D to get information on the nature of spatio-temporal dependencies
-# D0 has a nice interpretation as a proportional risk
-
-# Third diagnostic
-# D/sqrt(V) against K1*K2, similar to standardized residuals against actual values
-# Two dimensional, which is nice, but the spatio-temporal scales are no longer explicit
-
-
-## 5: Tests for space-time interactions
-# Use Q(s,t) which is basically hat K and also Knox with weights - correction for edge effects
-# U = \sum R(s,t)
-# MC test for U
-# Assumption of stationarity and radom permutations allow for non-stationarity
-
-
-## 6: Examples
-setwd("C:/gibbs/Baltimore")
 library(splancs)
 library(spatstat)
 
+
 # Function for automatic plots. See ?stmctest for explanation of parameters
-DigglePlots <- function(pts, times, poly, tlimits = c(0,1), s, tt, nsim = 100, quiet = FALSE){
+# Returns the output of stmctest
+DigglePlots <- function(pts, times, poly, tlimits = c(0,1), s, tt, nsim = 100, quiet = FALSE, Dzero = FALSE){
   if(missing(poly)){ poly <- bboxx(spoints(c(0,1,0,1)))}
   if(missing(s)){ s <- seq(0,0.7,0.01)[-1]}
   if(missing(tt)) { tt <- s }
@@ -53,13 +15,14 @@ DigglePlots <- function(pts, times, poly, tlimits = c(0,1), s, tt, nsim = 100, q
   stse <- stsecal(pts, times, poly, tlimits, s, tt)
   stmc <- stmctest(pts, times, poly, tlimits, s, tt, nsim, quiet)
   
-  stdiagn(pts, stkh, stse, stmc)
-  return(stkh)
+  stdiagn(pts, stkh, stse, stmc, Dzero)
+  return(stmc)
 }
 
 
+
 ## Poisson
-lambda <- 100
+lambda <- 1000
 poisson <- rpoispp3(lambda)
 plot(poisson, main = "Poisson", box.front=list(lty=5,col="red"), box.back=list(lty=5,col="red"), cex=0.8)
 distances <- seq(0,0.7,0.01)[-1]
@@ -67,7 +30,8 @@ times <- distances
 poisson.pts <- as.points(list(x = poisson$data$x, y = poisson$data$y))
 poisson.times <- poisson$data$z
 
-DigglePlots(poisson.pts, poisson.times)
+MC.Poisson <- DigglePlots(poisson.pts, poisson.times)
+
 paste("Poisson",toString(lambda),toString(nrow(poisson.pts)),sep="_")
 paste("Realization of a Poisson point process. Intensity ", str(lambda), ". Number of points: ", toString(nrow(poisson.pts)), sep="")
 paste("Diagnostic plots for a realization of a Poisson point process. Intensity ", str(lambda), ". Number of points: ", toString(nrow(poisson.pts)), sep="")
@@ -75,7 +39,7 @@ paste("Diagnostic plots for a realization of a Poisson point process. Intensity 
 
 
 ## Multidim Thomas process
-lambdap <- 40
+lambdap <- 20
 pois <- rpoispp3(lambdap)
 points <- matrix(c(pois$data$x,pois$data$y,pois$data$z), ncol=3)
 
@@ -93,8 +57,8 @@ generateChildren <- function(p,lambda = 10, sigma = 0.01){
 }
 
 
-lambdac <- 20
-sigmac <- 0.05
+lambdac <- 40
+sigmac <- 0.02
 
 Thomas <- apply(points,1,generateChildren, lambda = lambdac, sigma = sigmac)
 Thomas <- do.call(rbind,Thomas)
@@ -105,7 +69,7 @@ Thomas.times <- Thomas[,3]
 plot(pp3(Thomas.pts[,1],Thomas.pts[,2],Thomas.times,as.box3(c(0,1,0,1,0,1))), 
      main = "Thomas", box.front=list(lty=5,col="red"), box.back=list(lty=5,col="red"), cex=0.8)
 
-DigglePlots(Thomas.pts, Thomas.times)
+MC.Thomas <- DigglePlots(Thomas.pts, Thomas.times)
 
 paste("Thomas",toString(lambdap),toString(lambdac),gsub("\\.","p",toString(sigmac)),toString(nrow(Thomas.pts)),sep="_")
 paste("Realization of a Thomas point process. Intensity of parent process ", toString(lambdap), ". Child process: intensity ", lambdap,", distribution \\$N(0,", toString(sigmac),")\\$."," Number of points: ", toString(nrow(Thomas.pts)), ".", sep="")
@@ -136,14 +100,14 @@ for (i in 1:n){
 }
 points <- points[keep,]
 
-
 plot(pp3(points[,1],points[,2],points[,3],as.box3(c(0,1,0,1,0,1))), 
      main = "Matern", box.front=list(lty=5,col="red"), box.back=list(lty=5,col="red"), cex=0.8)
 
 Matern.pts <- as.points(list(x=points[,1],y=points[,2]))
 Matern.times <- points[,3] 
 
-DigglePlots(Matern.pts, Matern.times)
+MC.Mattern <- DigglePlots(Matern.pts, Matern.times)
+
 paste("Matern II",toString(lambda),gsub("\\.","p",toString(r)),toString(nrow(Matern.pts)),sep="_")
 paste("Realization of a Matern II point process. Intensity ", toString(lambda), ". Minimum distance ", toString(r), ". Number of points: ", toString(nrow(Matern.pts)), ".", sep="")
 paste("Diagnostic plots for a realization of a Matern II point process.  Intensity ", toString(lambda), ". Minimum distance ", toString(r), ". Number of points: ", toString(nrow(Matern.pts)), ".", sep="")
@@ -152,7 +116,7 @@ paste("Diagnostic plots for a realization of a Matern II point process.  Intensi
 
 
 
-# Baltimore data
+## Baltimore data
 balt <- readRDS("balt_standardized.rds")
 balt.pts <- as.points(list(x = balt$coords[,1], y = balt$coords[,2]))
 balt.times <- balt$times
@@ -160,15 +124,15 @@ balt.times <- balt$times
 plot(pp3(balt.pts[,1], balt.pts[,2], balt.times,as.box3(c(0,1,0,1,0,1))), 
      main = "Baltimore", box.front=list(lty=5,col="red"), box.back=list(lty=5,col="red"), cex=0.8)
 
-DigglePlots(balt.pts, balt.times)
+MC.balt <- DigglePlots(balt.pts, balt.times)
 
 
 # Baltimore data with no time change
 times.u <- unique(balt$times)
 coords.u <- unique(balt$coords)
 
-plot(pp3(coords.u[,1], coords.u[,2],rep(0.1,nrow(coords.u)), as.box3(c(0,1,0,1,0,1)) ), 
-     main = "Baltimore (all)", box.front=list(lty=5,col="red"), box.back=list(lty=5,col="red"), cex=0.8)
+# plot(pp3(coords.u[,1], coords.u[,2],rep(0.1,nrow(coords.u)), as.box3(c(0,1,0,1,0,1)) ), 
+#     main = "Baltimore (all)", box.front=list(lty=5,col="red"), box.back=list(lty=5,col="red"), cex=0.8)
 
 coords.all <- matrix(rep(c(t(coords.u)), length(times.u)), ncol=2, byrow = TRUE)
 times.all <- rep(unique(balt$times),each = nrow(coords.u))
@@ -176,19 +140,4 @@ times.all <- rep(unique(balt$times),each = nrow(coords.u))
 plot(pp3(coords.all[,1], coords.all[,2], times.all, as.box3(c(0,1,0,1,0,1))),
      main = "Baltimore (all)", box.front=list(lty=5,col="red"), box.back=list(lty=5,col="red"), cex=0.8)
 
-DigglePlots(coords.all, times.all)
-
-
-## Questions
-# Why is permutation test valid
-# Assumption of stationarity
-
-
-
-
-
-
-## Limitations for Baltimore dataset
-# time is hardly continuous
-# nonstationarity?
-# leaving out zero observations altogether 
+MC.baltall <- DigglePlots(coords.all, times.all)
